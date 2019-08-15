353824fb05bedd214a45ee3070f493ba40e30cb4
klupzor <lopezgand@gmail.com>
2019-08-15 02:13:35 +0000

    Creating user and review

--- models/user.py
+++ models/user.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+class User(BaseModel, Base):
+    """This is the class for User
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    cities = relationship('City',
+            cascade='all, delete', backref='state.id')
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
