b62a414782ee29d27bafdedea9c8856528463a85
klupzor <lopezgand@gmail.com>
2019-08-19 15:35:18 -0500

    setup server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Configuration
+dpkg -s nginx &> /dev/null  
+
+if [ $? -ne 0 ]
+then
+
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Testing Nginx" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "45i \\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
